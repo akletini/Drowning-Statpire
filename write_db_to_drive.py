d66ef947e92c43620c4021e90506533c523e48c6
Allen Kletinitch <allen.kletinitch@gmail.com>
2022-05-01 18:01:55 +0200

    Adjust file structure for drive upload

--- write_db_to_drive.py
+++ write_db_to_drive.py
@@ -11,6 +11,8 @@ gauth = GoogleAuth()
 gauth.LocalWebserverAuth()   
 drive = GoogleDrive(gauth)
 
+## Retrieve data from database
+
 con = sqlite3.connect('site.db')
 db_df = pd.read_sql_query("SELECT * FROM InstagramInfo", con)
 db_df.to_csv("data/instagram.csv")
@@ -21,14 +23,29 @@ db_df.to_csv("data/spotify.csv")
 db_df = pd.read_sql_query("SELECT * FROM YoutubeInfo", con)
 db_df.to_csv("data/youtube.csv")
 
+## Get root drive folder
+
+root_folder_name = "DrowningStatpire"
+folders = drive.ListFile(
+    {'q': "title='" + root_folder_name + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+root_folder = drive.CreateFile(
+    {
+       "title" : root_folder_name,
+       "mimeType" : "application/vnd.google-apps.folder",
+       "id": folders[0]["id"]
+    })
+
+## Write data to drive
+
 folder_name = "DrowningStatpireData"
 folders = drive.ListFile(
-    {'q': "title='" + folder_name + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+    {'q': "title='" + folder_name + "' and mimeType='application/vnd.google-apps.folder' and trashed=false and '" + root_folder["id"] + "' in parents" }).GetList()
 
 new_folder = drive.CreateFile(
     {
        "title" : folder_name,
-       "mimeType" : "application/vnd.google-apps.folder" 
+       "mimeType" : "application/vnd.google-apps.folder",
+       "parents" : [{'id': root_folder['id']}]
     })
 
 if len(folders) == 0:
@@ -38,7 +55,8 @@ else:
     {
        "title" : folder_name,
        "mimeType" : "application/vnd.google-apps.folder",
-       "id": folders[0]["id"]  
+       "id": folders[0]["id"],
+       "parents" : [{'id': root_folder['id']}]
     })
     old_folder.Trash()
     new_folder.Upload()
